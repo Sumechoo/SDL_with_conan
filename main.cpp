5131de9feb306b83d71021cce53f5cafaa3638a7
Gene Sumchenko <yevhenii.sumchenko@wickes.co.uk>
2022-04-22 11:06:24 +0300

    glfw variant

--- main.cpp
+++ main.cpp
@@ -1,28 +1,22 @@
 #include <iostream>
-#include <SDL2/SDL.h>
+#include <GLFW/glfw3.h>
 
 int main() {
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-        std::printf("error initializing SDL: %s\n", SDL_GetError());
+    if(!glfwInit()) {
+        printf("Can not init GLFW");
     }
 
-    SDL_Window* win = SDL_CreateWindow("Hello world", 100, 100, 620, 387, SDL_WINDOW_SHOWN);
-    if (win == NULL) {
-        std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
-        return EXIT_FAILURE;
+    GLFWwindow* window = glfwCreateWindow(640, 480, "My GLFW Title", NULL, NULL);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
     }
 
-    SDL_bool done = SDL_FALSE;
-
-    while (!done) {
-        SDL_Event event;
-
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                done = SDL_TRUE;
-            }
-        }
-    }
-
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }
